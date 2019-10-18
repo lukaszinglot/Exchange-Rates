a68deb3ca5fa661785f1d1393a5148757180f375
lukaszinglot <luk.inglot@gmail.com>
2019-10-18 06:57:21 +0200

    add site title in Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="site-header">
+      <p className="title">Kantor wymiany Walut</p>
+    </div>
+  );
+};
+
+export default Header;
