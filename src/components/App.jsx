36de3e0a4e1d57e93d860453142ff668d324c816
lukaszinglot <luk.inglot@gmail.com>
2019-10-18 06:46:42 +0200

    add main component, which open other components

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CurrencyModule from "./CurrencyModule";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <CurrencyModule />
+    </div>
+  );
+};
+
+export default App;
